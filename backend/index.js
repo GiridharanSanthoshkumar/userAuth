2e3ad7c6e9cd1768de121832d8a3539a730a5670
Giridharansanthoshkumar <giridharansanthosh0501@gmail.com>
2025-01-11 13:06:51 +0530

    cors1

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,17 @@ const userSchema = new mongoose.Schema({
 });
 const User = mongoose.model("User", userSchema);
 
-app.use(cors({ origin: ["https://user-auth-client-six.vercel.app"], methods:["POST","GET"],credentials: true }));
+
+app.use(
+  cors({
+    origin: "https://user-auth-client-six.vercel.app", // Your frontend URL
+    credentials: true, // Allow cookies to be sent
+  })
+);
+
+//app.use(cors({ origin: ["https://user-auth-client-six.vercel.app"], methods:["POST","GET"],credentials: true }));
+app.options("*", cors()); // Handle preflight requests
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
